6f68a7fa293c1e64e9248c652f658028245c7209
Scott Lee <scottlee5455@outlook.com>
2023-10-03 08:36:50 -0500

    Moving platform updates

--- ObstacleAssault/ObstacleGame/Intermediate/Build/Win64/UnrealEditor/Inc/ObstacleGame/UHT/MovingPlatform.gen.cpp
+++ ObstacleAssault/ObstacleGame/Intermediate/Build/Win64/UnrealEditor/Inc/ObstacleGame/UHT/MovingPlatform.gen.cpp
@@ -9,7 +9,6 @@
 PRAGMA_DISABLE_DEPRECATION_WARNINGS
 void EmptyLinkFunctionForGeneratedCodeMovingPlatform() {}
 // Cross Module References
-	COREUOBJECT_API UScriptStruct* Z_Construct_UScriptStruct_FVector();
 	ENGINE_API UClass* Z_Construct_UClass_AActor();
 	OBSTACLEGAME_API UClass* Z_Construct_UClass_AMovingPlatform();
 	OBSTACLEGAME_API UClass* Z_Construct_UClass_AMovingPlatform_NoRegister();
@@ -29,15 +28,6 @@ void EmptyLinkFunctionForGeneratedCodeMovingPlatform() {}
 #if WITH_METADATA
 		static const UECodeGen_Private::FMetaDataPairParam Class_MetaDataParams[];
 #endif
-#if WITH_METADATA
-		static const UECodeGen_Private::FMetaDataPairParam NewProp_MyVector_MetaData[];
-#endif
-		static const UECodeGen_Private::FStructPropertyParams NewProp_MyVector;
-#if WITH_METADATA
-		static const UECodeGen_Private::FMetaDataPairParam NewProp_MyX_MetaData[];
-#endif
-		static const UECodeGen_Private::FFloatPropertyParams NewProp_MyX;
-		static const UECodeGen_Private::FPropertyParamsBase* const PropPointers[];
 		static const FCppClassTypeInfoStatic StaticCppClassTypeInfo;
 		static const UECodeGen_Private::FClassParams ClassParams;
 	};
@@ -52,24 +42,6 @@ void EmptyLinkFunctionForGeneratedCodeMovingPlatform() {}
 		{ "ModuleRelativePath", "MovingPlatform.h" },
 	};
 #endif
-#if WITH_METADATA
-	const UECodeGen_Private::FMetaDataPairParam Z_Construct_UClass_AMovingPlatform_Statics::NewProp_MyVector_MetaData[] = {
-		{ "Category", "MovingPlatform" },
-		{ "ModuleRelativePath", "MovingPlatform.h" },
-	};
-#endif
-	const UECodeGen_Private::FStructPropertyParams Z_Construct_UClass_AMovingPlatform_Statics::NewProp_MyVector = { "MyVector", nullptr, (EPropertyFlags)0x0010000000000001, UECodeGen_Private::EPropertyGenFlags::Struct, RF_Public|RF_Transient|RF_MarkAsNative, nullptr, nullptr, 1, STRUCT_OFFSET(AMovingPlatform, MyVector), Z_Construct_UScriptStruct_FVector, METADATA_PARAMS(UE_ARRAY_COUNT(Z_Construct_UClass_AMovingPlatform_Statics::NewProp_MyVector_MetaData), Z_Construct_UClass_AMovingPlatform_Statics::NewProp_MyVector_MetaData) };
-#if WITH_METADATA
-	const UECodeGen_Private::FMetaDataPairParam Z_Construct_UClass_AMovingPlatform_Statics::NewProp_MyX_MetaData[] = {
-		{ "Category", "MovingPlatform" },
-		{ "ModuleRelativePath", "MovingPlatform.h" },
-	};
-#endif
-	const UECodeGen_Private::FFloatPropertyParams Z_Construct_UClass_AMovingPlatform_Statics::NewProp_MyX = { "MyX", nullptr, (EPropertyFlags)0x0010000000000001, UECodeGen_Private::EPropertyGenFlags::Float, RF_Public|RF_Transient|RF_MarkAsNative, nullptr, nullptr, 1, STRUCT_OFFSET(AMovingPlatform, MyX), METADATA_PARAMS(UE_ARRAY_COUNT(Z_Construct_UClass_AMovingPlatform_Statics::NewProp_MyX_MetaData), Z_Construct_UClass_AMovingPlatform_Statics::NewProp_MyX_MetaData) };
-	const UECodeGen_Private::FPropertyParamsBase* const Z_Construct_UClass_AMovingPlatform_Statics::PropPointers[] = {
-		(const UECodeGen_Private::FPropertyParamsBase*)&Z_Construct_UClass_AMovingPlatform_Statics::NewProp_MyVector,
-		(const UECodeGen_Private::FPropertyParamsBase*)&Z_Construct_UClass_AMovingPlatform_Statics::NewProp_MyX,
-	};
 	const FCppClassTypeInfoStatic Z_Construct_UClass_AMovingPlatform_Statics::StaticCppClassTypeInfo = {
 		TCppClassTypeTraits<AMovingPlatform>::IsAbstract,
 	};
@@ -79,16 +51,15 @@ void EmptyLinkFunctionForGeneratedCodeMovingPlatform() {}
 		&StaticCppClassTypeInfo,
 		DependentSingletons,
 		nullptr,
-		Z_Construct_UClass_AMovingPlatform_Statics::PropPointers,
+		nullptr,
 		nullptr,
 		UE_ARRAY_COUNT(DependentSingletons),
 		0,
-		UE_ARRAY_COUNT(Z_Construct_UClass_AMovingPlatform_Statics::PropPointers),
+		0,
 		0,
 		0x009000A4u,
 		METADATA_PARAMS(UE_ARRAY_COUNT(Z_Construct_UClass_AMovingPlatform_Statics::Class_MetaDataParams), Z_Construct_UClass_AMovingPlatform_Statics::Class_MetaDataParams)
 	};
-	static_assert(UE_ARRAY_COUNT(Z_Construct_UClass_AMovingPlatform_Statics::PropPointers) < 2048);
 	UClass* Z_Construct_UClass_AMovingPlatform()
 	{
 		if (!Z_Registration_Info_UClass_AMovingPlatform.OuterSingleton)
@@ -108,9 +79,9 @@ void EmptyLinkFunctionForGeneratedCodeMovingPlatform() {}
 		static const FClassRegisterCompiledInInfo ClassInfo[];
 	};
 	const FClassRegisterCompiledInInfo Z_CompiledInDeferFile_FID_Development_UE5_Course_GameDevTv_ObstacleAssault_ObstacleGame_Source_ObstacleGame_MovingPlatform_h_Statics::ClassInfo[] = {
-		{ Z_Construct_UClass_AMovingPlatform, AMovingPlatform::StaticClass, TEXT("AMovingPlatform"), &Z_Registration_Info_UClass_AMovingPlatform, CONSTRUCT_RELOAD_VERSION_INFO(FClassReloadVersionInfo, sizeof(AMovingPlatform), 153058581U) },
+		{ Z_Construct_UClass_AMovingPlatform, AMovingPlatform::StaticClass, TEXT("AMovingPlatform"), &Z_Registration_Info_UClass_AMovingPlatform, CONSTRUCT_RELOAD_VERSION_INFO(FClassReloadVersionInfo, sizeof(AMovingPlatform), 2448211990U) },
 	};
-	static FRegisterCompiledInInfo Z_CompiledInDeferFile_FID_Development_UE5_Course_GameDevTv_ObstacleAssault_ObstacleGame_Source_ObstacleGame_MovingPlatform_h_123276742(TEXT("/Script/ObstacleGame"),
+	static FRegisterCompiledInInfo Z_CompiledInDeferFile_FID_Development_UE5_Course_GameDevTv_ObstacleAssault_ObstacleGame_Source_ObstacleGame_MovingPlatform_h_2807959621(TEXT("/Script/ObstacleGame"),
 		Z_CompiledInDeferFile_FID_Development_UE5_Course_GameDevTv_ObstacleAssault_ObstacleGame_Source_ObstacleGame_MovingPlatform_h_Statics::ClassInfo, UE_ARRAY_COUNT(Z_CompiledInDeferFile_FID_Development_UE5_Course_GameDevTv_ObstacleAssault_ObstacleGame_Source_ObstacleGame_MovingPlatform_h_Statics::ClassInfo),
 		nullptr, 0,
 		nullptr, 0);
--- ObstacleAssault/ObstacleGame/Source/ObstacleGame/MovingPlatform.cpp
+++ ObstacleAssault/ObstacleGame/Source/ObstacleGame/MovingPlatform.cpp
@@ -15,7 +15,7 @@ AMovingPlatform::AMovingPlatform()
 void AMovingPlatform::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
 }
 
 // Called every frame
@@ -23,20 +23,16 @@ void AMovingPlatform::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	FVector LocalVector = MyVector;
-
-	LocalVector.Z = LocalVector.Z + 100;
-
-	MyVector.Y = MyVector.Y + 1;
-
-	SetActorLocation(LocalVector);
-
-	// Move platform forward
+	// Move platform forwards
 		// Get current location
+	FVector CurrentLocation = GetActorLocation();
 		// Add vector to that location
-		// Set that location
-	// Send platform back if gone too far
-		// Check how far we have moved
-		// Reverse direction of motion if gone too far
-}
+	CurrentLocation.X = CurrentLocation.X + 1;
+		// Set the location
+	SetActorLocation(CurrentLocation);
 
+	// Send platform back if gone too far
+		// Check how far we've moved
+		// Reverse direction of motion if gone too far
+
+}
