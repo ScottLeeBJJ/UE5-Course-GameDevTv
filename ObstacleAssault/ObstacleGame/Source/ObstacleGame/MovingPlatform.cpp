2cb6e1a0e7a7cbb77c8ebdd62cb842f487badf17
Scott Lee <scottlee5455@outlook.com>
2024-01-07 21:52:24 -0600

    Made the MovingPlatform move back and forth

--- ObstacleAssault/ObstacleGame/Source/ObstacleGame/MovingPlatform.cpp
+++ ObstacleAssault/ObstacleGame/Source/ObstacleGame/MovingPlatform.cpp
@@ -24,14 +24,22 @@ void AMovingPlatform::Tick(float DeltaTime)
 	// Move platform forwards
 		// Get current location
 	FVector CurrentLocation = GetActorLocation();
+
 		// Add vector to that location
 	CurrentLocation = CurrentLocation + PlatformVelocity * DeltaTime;
+
 		// Set the location
 	SetActorLocation(CurrentLocation);
 
 	// Send platform back if gone too far
 		// Check how far we've moved
-	DistanceMoved = FVector::Dist(StartLocation, CurrentLocation);
+	float DistanceMoved = FVector::Dist(StartLocation, CurrentLocation);
+
 		// Reverse direction of motion if gone too far
+	if (DistanceMoved > MoveDistance)
+	{
+		PlatformVelocity = -PlatformVelocity;
+		StartLocation = CurrentLocation;
+	}
 
 }
